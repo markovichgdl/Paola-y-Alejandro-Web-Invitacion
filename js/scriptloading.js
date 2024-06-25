4ddfd5e0f07a4dc56f6492badd3e5e13be16af6c
Markovich Gdl <136863287+markovichgdl@users.noreply.github.com>
2024-06-25 13:07:13 -0600

    depure_commit

--- js/scriptloading.js
+++ js/scriptloading.js
@@ -4,5 +4,5 @@ window.addEventListener("load", function () {
   var loader = document.getElementById("loader");
   var content = document.getElementById("content");
   loader.style.display = "none";
-  content.style.display = "block";
+  //content.style.display = "block";
 });
